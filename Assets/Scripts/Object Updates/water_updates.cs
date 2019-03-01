1b3b344e9549d43635dde74177610163159d7c2f
unknown <Administrator@CJC-METL-G03702.ad.ufl.edu>
2019-03-01 13:57:33 -0500

    Initial Commit

---
+++ Assets/Scripts/Object Updates/water_updates.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI; 
+using UnityEngine;
+
+public class water_updates : MonoBehaviour
+{
+
+    private GameObject slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = GameObject.Find("Slider");
+        gameObject.SetActive(false); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void updateHeight(){
+        if (slider.GetComponent<Slider>().value <= 1){
+            Vector3 position = new Vector3(0, 0, 0);
+            gameObject.transform.position = position;
+            gameObject.SetActive(false);  
+        } else {
+            gameObject.SetActive(true); 
+            Vector3 position = new Vector3(0, (slider.GetComponent<Slider>().value / 49.0f) * 2.0f, 0);
+            gameObject.transform.position = position; 
+        }
+        
+    }
+
+    public void setHeight(int height){
+        Vector3 position = new Vector3(0, height, 0);
+        gameObject.transform.position = position;
+    }
+
+}
---
+++ Assets/Scripts/UI Updates/inches_updater.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI; 
+using UnityEngine;
+
+public class inches_updater : MonoBehaviour
+{
+
+    private GameObject slider; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = GameObject.Find("Slider");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void updateInches(){
+        gameObject.GetComponent<Text>().text = "" + slider.GetComponent<Slider>().value;
+    }
+
+}
