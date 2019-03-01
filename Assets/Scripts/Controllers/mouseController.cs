b38bef18ef27f889f489c6d66689b7aad788c560
unknown <Administrator@CJC-METL-G03702.ad.ufl.edu>
2019-03-01 13:55:16 -0500

    Initial Commit

---
+++ Assets/Scripts/Controllers/mouseController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseController : MonoBehaviour
+{
+
+    float horizontalSpeed = 5.0f; 
+    float verticalSpeed = 5.0f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1)){
+            float h = horizontalSpeed * Input.GetAxis("Mouse X");
+            float v = verticalSpeed * Input.GetAxis("Mouse Y");
+            print (h + ", " + v);
+            if (Mathf.Abs(h) > Mathf.Abs(v))
+                transform.Rotate(0, -h, 0);
+            else
+                transform.Rotate(v, 0, 0);
+        }
+        else {
+            if (Input.mouseScrollDelta.y > 0){
+                transform.position += transform.forward; 
+            } 
+            else if (Input.mouseScrollDelta.y < 0) {
+                transform.position += -transform.forward; 
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Input/location_Input.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Mapbox.Unity.Map;
+
+public class location_Input : MonoBehaviour
+{
+
+	private GameObject citySimMap;
+	private AbstractMap mapOptions;
+	public GameObject lat, lon, ocean;
+	public Slider slider; 
+	private double latt, lonn; 
+	private bool change; 
+	//private Component[] components; 
+
+	void Awake(){
+        citySimMap = GameObject.Find("CitySimulatorMap");
+		latt = 0;
+		lonn = 0;
+		change = false; 
+		//components = citySimMap.GetComponents(typeof(Component));
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		mapOptions = citySimMap.GetComponent<AbstractMap>();
+		citySimMap.transform.position = new Vector3(0,0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void search(){
+		//Read input of coordinates and update the map with the new coordinates.  
+		latt = double.Parse(lat.GetComponent<Text>().text); 
+		lonn = double.Parse(lon.GetComponent<Text>().text);
+		mapOptions.SetCenterLatitudeLongitude(new Mapbox.Utils.Vector2d(latt,lonn));
+		mapOptions.UpdateMap(); 
+		//Reset dependent fields
+		ocean.GetComponent<water_updates>().setHeight(0); 
+		slider.value = 0;  
+		citySimMap.transform.position = new Vector3(0,0,0);
+		//Log change to console
+		print("Setting the coordinates to " + latt + ", " + lonn);
+	}
+}
